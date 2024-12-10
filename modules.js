5404d924059b598defd2091f78a81174e97f78be
hanumangod <ashraypatel1111@gmail.com>
2024-12-10 16:54:22 +0530

    first node repo

---
+++ modules.js
@@ -0,0 +1,9 @@
+
+const names = require('./names')
+console.log(names)
+
+const sayHi = require('./utils')
+
+sayHi("sarah")
+sayHi(names.john)
+sayHi(names.peter)
---
+++ names.js
@@ -0,0 +1,6 @@
+const john = "john"
+const peter = "peter"
+const secret = "supper secret"
+
+
+module.exports = {john, peter}
